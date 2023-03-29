3e1523b2e70f6f6c252ec855947698f0dfc964eb
Gabriel <estudo.gabmartts@gmail.com>
2023-03-29 17:59:22 -0300

    List Cleaned

--- src/components/lista/Lista.tsx
+++ src/components/lista/Lista.tsx
@@ -2,16 +2,14 @@ import { Lista } from './Lista.types'
 import styles from '@/styles/Home.module.css'
 import { useState } from 'react'
 export function Lista({data}: any){
-  const [useItem, setItem] = useState("")
     return(
         <>
         {data.map((e: any) => e.rankE.map((e: Lista, index: number) => (
-                <tr className={styles.list} key={index}>
+                <tr key={index}>
                 <td>{e['Item']}</td>
                 <td>{e['Habilidade']}</td>
                 <td>{e['Tempo de espera']}</td>
                 <td >
-                {useItem && <p>{useItem}</p>}
                   {e['Materiais'].map((m, index) => (
                     <>
                     <p key={index}>{m.quantity}, {m.name}</p>
