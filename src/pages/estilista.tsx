eae9f6a831cac1b8bd14c7a0d0cf2ef371c1b5de
Gabriel <estudo.gabmartts@gmail.com>
2023-03-30 18:04:54 -0300

    Page Estilista

--- src/pages/estilista.tsx
+++ src/pages/estilista.tsx
@@ -1,5 +1,6 @@
+import { Lista } from '@/components/lista'
 import styles from '@/styles/Home.module.css'
-
+import data from '../json/estilista/betterEstilista.json'
 export default function Estilista () {
     return(
         <main className={styles.main}>
@@ -9,8 +10,8 @@ export default function Estilista () {
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
         <h1>Estilista</h1>
-
           </div>
+          <Lista data={data}/>
 
         </main>
     )
