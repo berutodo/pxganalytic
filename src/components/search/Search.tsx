6a3c372f3eaf1e2f4be4fe4eab14e8959631b79b
Gabriel <estudo.gabmartts@gmail.com>
2023-05-21 22:37:09 -0300

    Search icon

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image"
+import searchSvg from "../../helpers/icons/search.svg"
 export function Search () {
     return (
-        <div className="flex justify-center mt-32">
-        <input className="w-96 h-10 border-2 mb-10 border-slate-500 rounded-lg " type="text" />
+        <div className="flex justify-center align-center row mt-32">
+        <input className="w-96 h-10 border-2 border-slate-500 rounded-lg " type="text" />
+        <Image src={searchSvg} width={32} height={32} alt="" />
         </div>
     )
 }
