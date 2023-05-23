c668006ef3833af33ec5169875e926fca74e132d
Gabriel <estudo.gabmartts@gmail.com>
2023-05-23 18:29:20 -0300

    Fixed spaces json names

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import searchSvg from "../../helpers/icons/search.svg"
 export function Search () {
     return (
-        <div className="flex justify-center align-center row mt-32">
+        <div className="flex justify-center mb-10 align-center row mt-32">
         <input className="w-96 h-10 border-2 border-slate-500 rounded-lg " type="text" />
         <Image src={searchSvg} width={32} height={32} alt="" />
         </div>
