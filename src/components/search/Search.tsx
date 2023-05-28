847488d474a76c2e83390ab5512c5b340e7cc0d6
Gabriel <estudo.gabmartts@gmail.com>
2023-05-28 20:26:22 -0300

    Sopmethign

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -9,9 +9,11 @@ export function Search ({onSearch}: any) {
         onSearch(searchValue); // Chame a função do componente pai com os resultados da busca
       };
     return (
-        <div className="flex justify-center mb-10 align-center row mt-32">
-        <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="w-96 h-10 border-2 border-slate-500 rounded-lg " type="text" />
-        <Image src={searchSvg} width={32} height={32} alt="" onClick={handleSearch} />
+        <div className="flex justify-center mb-10 align-center items-center row mt-32">
+        <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="w-96 h-10 border-2 border-slate-500 rounded-l-lg " type="text" />
+        <div className="cursor-pointer border-2 border-slate-500 border-l-0 rounded-r-lg w-14 flex items-center justify-center h-10 ">
+        <Image  src={searchSvg} width={28} height={28} alt="" onClick={handleSearch} />
+        </div>
         </div>
     )
 }
