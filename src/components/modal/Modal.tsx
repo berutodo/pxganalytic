ca2c262a40db9f705a0e228773df97751637c494
Gabriel <estudo.gabmartts@gmail.com>
2023-06-04 16:43:12 -0300

    Fixing fetching

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -6,20 +6,25 @@ export function Modal({ x }: any) {
   const pokemons = lista.find(e => e.name == x)?.pokemons
   useEffect(() => {
     const fetchData = async () => {
-      pokemons?.forEach(pokemon => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.title.toLowerCase()}`)
-          .then(response => response.json())
-          .then(data => {
-            const image_url = data.sprites.front_default
-            setSprite(prevImages => [...prevImages, image_url])
-          })
-          .catch(error => {
-            console.log(`Erro ao buscar o item ${pokemon.title}:`, error);
-          });
-      });
-    }
+      if (sprite.length === 0) { // Verifica se já existem imagens no estado sprite
+        const spritePromises = pokemons?.map((pokemon) =>
+          fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.title.toLowerCase()}`)
+            .then((response) => response.json())
+            .then((data) => data.sprites.front_default)
+            .catch((error) => {
+              console.log(`Erro ao buscar o item ${pokemon.title}:`, error);
+              return null; // Trata o erro e retorna um valor padrão
+            })
+        );
+  
+        const spriteResults = await Promise.all(spritePromises);
+        setSprite(spriteResults.filter((sprite) => sprite !== null));
+      }
+    };
+  
     fetchData();
-  },[])
+  }, [pokemons, sprite]); // Adiciona sprite como uma dependência do useEffect
+  
   
     return (
       <div className="w-96 h-96  z-10 border-black flex flex-col justify-center items-center border-2 mx-auto fixed">
