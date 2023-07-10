a1e9f7a4528ebc534a53794ef5458be77a680368
Gabriel <estudo.gabmartts@gmail.com>
2023-07-10 17:09:56 -0300

    Adventurer S

--- src/components/lista/Lista.tsx
+++ src/components/lista/Lista.tsx
@@ -21,16 +21,19 @@ export function Lista({data, searchData}: any){
         <div onClick={handleOutsideClick} className='relative w-full flex flex-row justify-center'>
         {statusModal && <Modal x={searchedItem} />}
         </div>
-        {data.map((e: any) => e.rankE.map((e: Lista, index: number) => (
+        {data.map((e: any) => e.rankS.map((e: Lista, index: number) => (
                 <div className='flex flex-row p-4 justify-around shadow-sm cursor-pointer w-11/12 sm:w-1/3 h-32 items-center border-2 rounded-md relative' key={index}>
+                  <div>
+                  <Image className='min-w-max h-auto' alt={e['Item']} sizes='100vw' width={0} height={0} src={`https://wiki.pokexgames.com${e.src}`}/>
                   <h1>{e['Item']}</h1>
+                  </div>
                   <h1 className='top-1 right-1 absolute'>{e['Habilidade']}</h1>
                   <h1 className='bottom-1 right-1 absolute'>{e['Tempo de espera']}</h1>
-                  <div className='flex flex-row gap-4'>
+                  <div className='flex flex-row flex-wrap gap-4'>
                     {e['Materiais'].map((m, index) => (
-                      <div key={index} onClick={() => handleClick(m.name)} className='flex flex-row items-baseline'>
+                      <div key={index} onClick={() => handleClick(m.name)} className='flex min-w-min flex-row items-baseline'>
                       <p>{m.quantity}</p>
-                      <Image alt={m.name} title={m.name} width={20} height={20} src={lista.find((obj) => obj.name === m.name)?.src ?? ""} />
+                      <Image className='w-full h-auto' alt={m.name} title={m.name} sizes='100vw' width={0} height={0} src={lista.find((obj) => obj.name === m.name)?.src ?? ""} />
                       </div>
                     ))}
                   </div>
