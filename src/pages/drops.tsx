61c1fcc60de24b729fe7cb6ecfe44ecd70477d94
berutodo <estudo.gabmartts@gmail.com>
2024-05-07 22:25:57 -0300

    test: Images size

--- src/pages/drops.tsx
+++ src/pages/drops.tsx
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
+import Image from 'next/image';
 
 interface PokemonData {
   name: string,
@@ -48,17 +49,17 @@ export default function Drops({newData, itemsData}: {newData: PokemonData[], ite
         <div>
         <div className="flex p-20 flex-wrap w-4/5 mx-auto justify-center gap-4 font-WorkSans">
         {newData.map(pokemon => 
-          <div className="flex flex-col w-80 items-center justify-center gap-14 rounded-md border-2 border-black p-6">
-          <div><img src={pokemon.src} alt="" /></div>
+          <div key={pokemon.name} className="flex flex-col w-80 items-center justify-center gap-14 rounded-md border-2 border-black p-6">
+          <div className='relative w-[96px] h-[96px]'><Image fill src={pokemon.src} alt={pokemon.name} /></div>
           <div className="w-full">
           {pokemon.drops.map(drop => {
             const image = itemsData.find(item => item.name === drop.name)
             const src = image ? image.src : "";
 
             return (
-              <div className="flex p-2 items-center justify-between border-b-2 border-gray-300">
+              <div key={drop.name} className="flex p-2 items-center justify-between border-b-2 border-gray-300">
                 <div className="flex items-center gap-2">
-                  <div className="w-8 flex justify-center"><img height={40} src={src} alt={drop.name} /></div>
+                  <div className="relative w-4 h-5 flex justify-center"><Image fill={true} src={src} alt={drop.name} /></div>
                   <div>
                     <h1>{drop.name}</h1>
                   </div>
