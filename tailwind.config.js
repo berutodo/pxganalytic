6aba1b60f4458c25540d8ffe23e387e674e2bb2e
berutodo <estudo.gabmartts@gmail.com>
2024-05-09 13:15:27 -0300

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
         "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                WorkSans: ["Work Sans", "sans-serif"]
+            }
+        },
     },
     plugins: [],
 }
